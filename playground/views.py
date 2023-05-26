eb971f318cd743aeedb328e2c74ad3c0a3a8a53a
OTonGitHub <ahmed.ifhaam.work@outlook.com>
2023-05-27 02:15:09 +0500

    Added mapping documentation in app view

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,22 @@
+# reuquest -> response: request handler
+# action
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse # explicit, handling request (actions)
 
 def calculate():
     x = 10
     y = 20
     return x * y
+
+# Map ->
+    # ./urls.py [create_anyName] (., ../startproj [default])
+    # import path & .views
+    # set urlpatters []
+    # path('hello/', view.say_hello) # not function call
+    # path('playground/', include('playground.urls')) & import django.url.path, include
+    # urlpath: str, pointer to function -> HttpResponseBase Object
+    # end route "/" !
 
 def say_hello(request):
     # example things to do here:
@@ -15,5 +27,7 @@ def say_hello(request):
     # x = 1 # debugging, bp
     # y = 1
     value = calculate() # debug, step into
+    # request, template, dictionary (str, any obj [includes, model, string, int array etc])
     return render(request, 'hello.html', {"name": "OT", "value": value}) # returns HttpResponseObject
         # render: HttpRequst, Template, str Dictionary
+        # retuns  HttpReponse as promised in path() of urlpatterns
